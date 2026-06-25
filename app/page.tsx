677357b7ca245dcdfd9e03a830ee9549c140cde8
tech5-opti <tech5@optiminastic.com>
2026-06-25 14:27:43 +0530

    added DB

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,10 @@ import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
 import BlogShowcase from "@/app/components/BlogShowcase";
 import Footer from "@/app/components/Footer";
+import LatestFromBlog from "@/app/components/LatestFromBlog";
 import { getAllPosts } from "@/app/lib/posts";
+
+export const revalidate = 300;
 
 export default function Home() {
   const posts = getAllPosts();
@@ -11,6 +14,7 @@ export default function Home() {
     <>
       <Navbar />
       <main className="flex-1">
+        <LatestFromBlog />
         <Hero />
         <BlogShowcase posts={posts} />
       </main>
